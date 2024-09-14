2fe9324ab8a57e6bf3707ef564b6d26a63f16deb
barsa <barshapriyadarshini0602@gmail.com>
2024-09-14 08:50:28 +0530

    Spring Expression language using static method, value and object

--- springCore/src/main/java/com/springCore/spEL/Demo.java
+++ springCore/src/main/java/com/springCore/spEL/Demo.java
@@ -9,9 +9,52 @@ public class Demo {
   private int x;
   @Value("#{2+24+50}")
   private int y;
+  
+  //static method
+  
+  @Value("#{T(java.lang.Math).sqrt(144)}")
+  private int z;
+  
+  //static variable
+  @Value("#{T(java.lang.Math).PI}")
+  private double e;
+  
+  //using object
+  @Value("#{new java.lang.String('Barsa Jena')}")
+  private String name;
+  
+  //boolean type value
+  @Value("#{8 > 3}")
+  private boolean isActive;
+  
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public boolean isActive() {
+	return isActive;
+}
+public void setActive(boolean isActive) {
+	this.isActive = isActive;
+}
+public double getE() {
+	return e;
+}
+public void setE(double e) {
+	this.e = e;
+}
+public int getZ() {
+	return z;
+}
+public void setZ(int z) {
+	this.z = z;
+}
 public int getX() {
 	return x;
 }
+
 public void setX(int x) {
 	this.x = x;
 }
@@ -23,8 +66,6 @@ public void setY(int y) {
 }
 @Override
 public String toString() {
-	return "Demo [x=" + x + ", y=" + y + "]";
+	return "Demo [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", name=" + name + ", isActive=" + isActive + "]";
 }
-  
-  
 }
